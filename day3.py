87a2b8683f0723f224824e4e2439a990bf85e8ed
Robin Michael <Siliconrob@siliconheaven.net>
2022-12-03 12:21:09 -0600

    alphabet standard

--- day3.py
+++ day3.py
@@ -1,3 +1,4 @@
+import string
 from aocd import get_data
 import more_itertools as mit
 
@@ -17,20 +18,17 @@ def chunk(in_string, num_chunks):
         yield ''.join(accumulator)
 
 
-chars: str = 'abcdefghijklmnopqrstuvwxyz'
-alphabet: list[str] = ''.join([chars, chars.upper()])
-
-
-def part1_priority(input_line: str) -> list[int]:
+def part1_priority(alphabet: str, input_line: str) -> list[int]:
     chunks = list(chunk(input_line, 2))
     return alphabet.index((set(chunks[0]) & set(chunks[1])).pop()) + 1
 
 
 # https://stackoverflow.com/questions/312443/how-do-i-split-a-list-into-equally-sized-chunks
-def part2_priorities(input_lines: list[str]) -> list[int]:
+def part2_priorities(alphabet: str, input_lines: list[str]) -> list[int]:
     return [(alphabet.index((set(basket[0]) & set(basket[1]) & set(basket[2])).pop()) + 1) for basket in list(mit.batched(input_lines, 3))]
 
 if __name__ == '__main__':
+    data = get_data(day=3, year=2022).splitlines()
     # data = ['vJrwpWtwJgWrhcsFMMfFFhFp',
     #         'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
     #         'PmmdzqPrVvPwwTWBwg',
@@ -38,9 +36,10 @@ if __name__ == '__main__':
     #         'ttgJtRGJQctTZtZT',
     #         'CrZsJsPPZsGzwwsLwLmpwMDw'
     # ]
-    data = get_data(day=3, year=2022).splitlines()
-    part1_priorities = [part1_priority(input_line) for input_line in data]
+
+    alphabet: str = ''.join([string.ascii_lowercase, string.ascii_uppercase])
+    part1_priorities = [part1_priority(alphabet, input_line) for input_line in data]
     print(f'Part 1: {sum(part1_priorities)}')
 
-    part2_answer = part2_priorities(data)
+    part2_answer = part2_priorities(alphabet, data)
     print(f'Part 2: {sum(part2_answer)}')
