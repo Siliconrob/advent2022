b40cf1f5001988fec16fb1394ab958c1676d11d7
Robin Michael <Siliconrob@siliconheaven.net>
2022-12-03 23:24:11 -0600

    Part 2 done

--- day4.py
+++ day4.py
@@ -1,27 +1,13 @@
-import string
 from dataclasses import dataclass
 from aocd import get_data
-import more_itertools as mit
 from parse import parse
+
 
 @dataclass
 class ElfAssignments:
     Elf1: set
     Elf2: set
 
-
-def part1_priority(alphabet: str, input_line: str) -> list[int]:
-    chunk1, chunk2 = mit.divide(2, list(input_line))
-    common = set(''.join(list(chunk1))) & set(''.join(list(chunk2)))
-    return alphabet.index(common.pop()) + 1
-
-
-# https://stackoverflow.com/questions/312443/how-do-i-split-a-list-into-equally-sized-chunks
-def part2_priorities(alphabet: str, input_lines: list[str]) -> list[int]:
-    return [(alphabet.index((set(basket[0]) & set(basket[1]) & set(basket[2])).pop()) + 1) for basket in list(mit.batched(input_lines, 3))]
-
-# def expand_range(elf_range_assignment: str) -> str:
-#      = parse('{} {}', elf_pair_input)
 
 def parse_line(elf_pair_input: str):
     elf1_range_start, elf1_range_end, elf2_range_start, elf2_range_end = parse('{:d}-{:d},{:d}-{:d}', elf_pair_input)
@@ -40,11 +26,13 @@ if __name__ == '__main__':
     #         '2-6,4-8']
 
     part1_matches = 0
+    part2_matches = 0
     for input_line in data:
         assignment = parse_line(input_line)
         matches = assignment.Elf1 & assignment.Elf2
         if (matches & assignment.Elf1 == assignment.Elf1) or (matches & assignment.Elf2 == assignment.Elf2):
             part1_matches += 1
-        #print(matches)
+        elif len(matches) > 0:
+            part2_matches += 1
     print(f'Part 1: {part1_matches}')
-
+    print(f'Part 2: {part1_matches + part2_matches}')
