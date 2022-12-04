08b1d9c6efaec4b20f8fc1b4bfd1768e714c7903
Robin Michael <Siliconrob@siliconheaven.net>
2022-12-03 23:21:30 -0600

    Part 1 done

--- day4.py
+++ day4.py
@@ -1,46 +1,50 @@
+import string
+from dataclasses import dataclass
 from aocd import get_data
 import more_itertools as mit
+from parse import parse
+
+@dataclass
+class ElfAssignments:
+    Elf1: set
+    Elf2: set
 
 
-# https://stackoverflow.com/questions/22571259/split-a-string-into-n-equal-parts
-def chunk(in_string, num_chunks):
-    chunk_size = len(in_string) // num_chunks
-    if len(in_string) % num_chunks: chunk_size += 1
-    iterator = iter(in_string)
-    for _ in range(num_chunks):
-        accumulator = list()
-        for _ in range(chunk_size):
-            try:
-                accumulator.append(next(iterator))
-            except StopIteration:
-                break
-        yield ''.join(accumulator)
-
-
-chars: str = 'abcdefghijklmnopqrstuvwxyz'
-alphabet: list[str] = ''.join([chars, chars.upper()])
-
-
-def part1_priority(input_line: str) -> list[int]:
-    chunks = list(chunk(input_line, 2))
-    return alphabet.index((set(chunks[0]) & set(chunks[1])).pop()) + 1
+def part1_priority(alphabet: str, input_line: str) -> list[int]:
+    chunk1, chunk2 = mit.divide(2, list(input_line))
+    common = set(''.join(list(chunk1))) & set(''.join(list(chunk2)))
+    return alphabet.index(common.pop()) + 1
 
 
 # https://stackoverflow.com/questions/312443/how-do-i-split-a-list-into-equally-sized-chunks
-def part2_priorities(input_lines: list[str]) -> list[int]:
+def part2_priorities(alphabet: str, input_lines: list[str]) -> list[int]:
     return [(alphabet.index((set(basket[0]) & set(basket[1]) & set(basket[2])).pop()) + 1) for basket in list(mit.batched(input_lines, 3))]
 
-if __name__ == '__main__':
-    data = ['vJrwpWtwJgWrhcsFMMfFFhFp',
-            'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
-            'PmmdzqPrVvPwwTWBwg',
-            'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
-            'ttgJtRGJQctTZtZT',
-            'CrZsJsPPZsGzwwsLwLmpwMDw'
-    ]
-    #data = get_data(day=4, year=2022).splitlines()
-    part1_priorities = [part1_priority(input_line) for input_line in data]
-    print(f'Part 1: {sum(part1_priorities)}')
+# def expand_range(elf_range_assignment: str) -> str:
+#      = parse('{} {}', elf_pair_input)
 
-    part2_answer = part2_priorities(data)
-    print(f'Part 2: {sum(part2_answer)}')
+def parse_line(elf_pair_input: str):
+    elf1_range_start, elf1_range_end, elf2_range_start, elf2_range_end = parse('{:d}-{:d},{:d}-{:d}', elf_pair_input)
+    elf1_assignment = set(list(range(elf1_range_start, elf1_range_end + 1)))
+    elf2_assignment = set(list(range(elf2_range_start, elf2_range_end + 1)))
+    return ElfAssignments(elf1_assignment, elf2_assignment)
+
+
+if __name__ == '__main__':
+    data = get_data(day=4, year=2022).splitlines()
+    # data = ['2-4,6-8',
+    #         '2-3,4-5',
+    #         '5-7,7-9',
+    #         '2-8,3-7',
+    #         '6-6,4-6',
+    #         '2-6,4-8']
+
+    part1_matches = 0
+    for input_line in data:
+        assignment = parse_line(input_line)
+        matches = assignment.Elf1 & assignment.Elf2
+        if (matches & assignment.Elf1 == assignment.Elf1) or (matches & assignment.Elf2 == assignment.Elf2):
+            part1_matches += 1
+        #print(matches)
+    print(f'Part 1: {part1_matches}')
+
