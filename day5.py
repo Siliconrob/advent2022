4bdec447d6e12d247c74f3ff0048d7ef310a36eb
Robin Michael <Siliconrob@siliconheaven.net>
2022-12-05 00:24:38 -0600

    Part 2

--- day5.py
+++ day5.py
@@ -1,27 +1,7 @@
 import re
-import string
-from dataclasses import dataclass
+from copy import deepcopy
 from aocd import get_data
 from parse import parse
-
-@dataclass
-class ElfAssignments:
-    Elf1: set
-    Elf2: set
-
-    def common_elements(self) -> set:
-        return self.Elf1 & self.Elf2
-
-    def is_match(self, set_to_match: set) -> bool:
-        return self.common_elements() & set_to_match == set_to_match
-
-
-def parse_line(elf_pair_input: str) -> ElfAssignments:
-    parse_result = parse('{:d}-{:d},{:d}-{:d}', elf_pair_input).fixed
-    elf1_assignment = set(list(range(parse_result[0], parse_result[1] + 1)))
-    elf2_assignment = set(list(range(parse_result[2], parse_result[3] + 1)))
-    return ElfAssignments(elf1_assignment, elf2_assignment)
-
 
 if __name__ == '__main__':
     data = [
@@ -36,14 +16,13 @@ if __name__ == '__main__':
 'move 1 from 1 to 2',
 ]
 
-    # data = get_data(day=5, year=2022).splitlines()
+    data = get_data(day=5, year=2022).splitlines()
 
     splitter_index = data.index('')
     initial_state = data[:splitter_index - 1]
     positions = data[splitter_index - 1]
     stack_numbers = [int(i) for i in re.findall("\d+", positions)]
     stacks_to_make = range(1, max(stack_numbers) + 1)
-
 
     position_lookup = {}
     stacks = {}
@@ -58,15 +37,30 @@ if __name__ == '__main__':
             if initial_state_row[value] != ' ':
                 stack.append(initial_state_row[value])
 
+
     commands = data[splitter_index + 1:]
+    part1_stack_start = deepcopy(stacks)
+    part2_stack_start = deepcopy(stacks)
+
     for command_input in commands:
         parsed_command = parse('move {:d} from {:d} to {:d}', command_input).fixed
-        # print(parsed_command)
         for crates in range(1, parsed_command[0] + 1):
-            source_stack = stacks[parsed_command[1]]
-            target_stack = stacks[parsed_command[2]]
+            source_stack = part1_stack_start[parsed_command[1]]
+            target_stack = part1_stack_start[parsed_command[2]]
             target_stack.append(source_stack.pop())
 
-    print(stacks)
-    completed = ''.join([completed_stack.pop() for completed_stack in stacks.values()])
+    completed = ''.join([completed_stack.pop() for completed_stack in part1_stack_start.values()])
     print(f'Part 1: {completed}')
+
+    for command_input in commands:
+        parsed_command = parse('move {:d} from {:d} to {:d}', command_input).fixed
+        source_stack = part2_stack_start[parsed_command[1]]
+        target_stack = part2_stack_start[parsed_command[2]]
+        to_move = []
+        for crates in range(1, parsed_command[0] + 1):
+            to_move.append(source_stack.pop())
+        to_move.reverse()
+        target_stack.extend(to_move)
+
+    completed = ''.join([completed_stack.pop() for completed_stack in part2_stack_start.values()])
+    print(f'Part 2: {completed}')
