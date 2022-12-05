33131381650b081be45ff27bba461a02a2025995
Robin Michael <Siliconrob@siliconheaven.net>
2022-12-05 00:05:55 -0600

    Part 1 complete

--- day5.py
+++ day5.py
@@ -1,7 +1,8 @@
+import re
+import string
 from dataclasses import dataclass
 from aocd import get_data
 from parse import parse
-
 
 @dataclass
 class ElfAssignments:
@@ -23,22 +24,49 @@ def parse_line(elf_pair_input: str) -> ElfAssignments:
 
 
 if __name__ == '__main__':
-    # data = get_data(day=5, year=2022).splitlines()
-    data = ['2-4,6-8',
-            '2-3,4-5',
-            '5-7,7-9',
-            '2-8,3-7',
-            '6-6,4-6',
-            '2-6,4-8']
+    data = [
+'    [D]    ',
+'[N] [C]    ',
+'[Z] [M] [P]',
+' 1   2   3 ',
+'',
+'move 1 from 2 to 1',
+'move 3 from 1 to 3',
+'move 2 from 2 to 1',
+'move 1 from 1 to 2',
+]
 
-    part1_matches = 0
-    part2_matches = 0
-    for input_line in data:
-        assignment = parse_line(input_line)
-        matches = assignment.common_elements()
-        if assignment.is_match(assignment.Elf1) or assignment.is_match(assignment.Elf2):
-            part1_matches += 1
-        elif len(matches) > 0:
-            part2_matches += 1
-    print(f'Part 1: {part1_matches}')
-    print(f'Part 2: {part1_matches + part2_matches}')
+    # data = get_data(day=5, year=2022).splitlines()
+
+    splitter_index = data.index('')
+    initial_state = data[:splitter_index - 1]
+    positions = data[splitter_index - 1]
+    stack_numbers = [int(i) for i in re.findall("\d+", positions)]
+    stacks_to_make = range(1, max(stack_numbers) + 1)
+
+
+    position_lookup = {}
+    stacks = {}
+    for stack_number in stacks_to_make:
+        position_lookup[stack_number] = positions.index(str(stack_number))
+        stacks[stack_number] = []
+
+    initial_state.reverse()
+    for initial_state_row in initial_state:
+        for key, value in position_lookup.items():
+            stack = [] if stacks.get(key) is None else stacks.get(key)
+            if initial_state_row[value] != ' ':
+                stack.append(initial_state_row[value])
+
+    commands = data[splitter_index + 1:]
+    for command_input in commands:
+        parsed_command = parse('move {:d} from {:d} to {:d}', command_input).fixed
+        # print(parsed_command)
+        for crates in range(1, parsed_command[0] + 1):
+            source_stack = stacks[parsed_command[1]]
+            target_stack = stacks[parsed_command[2]]
+            target_stack.append(source_stack.pop())
+
+    print(stacks)
+    completed = ''.join([completed_stack.pop() for completed_stack in stacks.values()])
+    print(f'Part 1: {completed}')
