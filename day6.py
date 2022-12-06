92339c2c0a101107a85e808f39232d09007f9192
Robin Michael <Siliconrob@siliconheaven.net>
2022-12-05 23:33:22 -0600

    Part 2 complete

--- day6.py
+++ day6.py
@@ -1,61 +1,27 @@
-import itertools
-import re
-from collections import deque
-from copy import deepcopy
 from aocd import get_data
-from parse import parse
-
 
 if __name__ == '__main__':
-    data = [
-        'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg',
-    ]
+    # data = [
+    #     'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw',
+    # ]
     data = get_data(day=6, year=2022).splitlines()
     packet_marker_size = 4
     current_index = 0
     input = data[0]
 
-    # for each in itertools.permutations(input, 4):
-    #     print(each)
-
     while current_index + packet_marker_size < len(input):
-        packet = set(input[current_index:current_index+packet_marker_size])
+        packet = set(input[current_index:current_index + packet_marker_size])
         if len(packet) == packet_marker_size:
             break
         current_index += 1
     print(f'Part 1: {current_index + packet_marker_size}')
 
+    start_of_message = 14
+    current_index = 0
 
-
-
-    #
-    # for index in data[0]:
-    #     print(index)
-    #
-    #
-    # while len(stream_reader) < packet_marker_size:
-    #     current_char = input_stream.popleft()
-    #     if current_char in stream_reader:
-    #         if len(stream_reader) > 0:
-    #             stream_reader.popleft()
-    #     else:
-    #         stream_reader.append(current_char)
-    #     current_index += 1
-    #
-    # print(f'Part 1: {current_index - 1}')
-
-
-
-
-
-
-    # splitter_index = data.index('')
-    # initial_state = data[:splitter_index - 1]
-    # commands = data[splitter_index + 1:]
-    #
-    # stack_start = build_initial_state(initial_state)
-    # part1_stack_start = deepcopy(stack_start)
-    # part2_stack_start = deepcopy(stack_start)
-    #
-    # print(f'Part 1: {part1(part1_stack_start, commands)}')
-    # print(f'Part 2: {part2(part2_stack_start, commands)}')
+    while current_index + start_of_message < len(input):
+        packet = set(input[current_index:current_index + start_of_message])
+        if len(packet) == start_of_message:
+            break
+        current_index += 1
+    print(f'Part 2: {current_index + start_of_message}')
