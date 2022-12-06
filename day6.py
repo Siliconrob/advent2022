294bcdda61de42fb9e7826efb10419e52a37f038
Robin Michael <Siliconrob@siliconheaven.net>
2022-12-05 23:44:24 -0600

    Cleanup

--- day6.py
+++ day6.py
@@ -1,27 +1,24 @@
 from aocd import get_data
 
+
+def read_input_stream(packet_length, input_line):
+    current_index = 0
+    while current_index + packet_length < len(input_line):
+        packet = set(input_line[current_index:current_index + packet_length])
+        if len(packet) == packet_length:
+            break
+        current_index += 1
+    return current_index + packet_length
+
+
 if __name__ == '__main__':
     # data = [
     #     'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw',
     # ]
+
     data = get_data(day=6, year=2022).splitlines()
-    packet_marker_size = 4
-    current_index = 0
-    input = data[0]
+    part1_index = read_input_stream(4, data[0])
+    print(f'Part 1: {part1_index}')
 
-    while current_index + packet_marker_size < len(input):
-        packet = set(input[current_index:current_index + packet_marker_size])
-        if len(packet) == packet_marker_size:
-            break
-        current_index += 1
-    print(f'Part 1: {current_index + packet_marker_size}')
-
-    start_of_message = 14
-    current_index = 0
-
-    while current_index + start_of_message < len(input):
-        packet = set(input[current_index:current_index + start_of_message])
-        if len(packet) == start_of_message:
-            break
-        current_index += 1
-    print(f'Part 2: {current_index + start_of_message}')
+    part2_index = read_input_stream(14, data[0])
+    print(f'Part 2: {part2_index}')
