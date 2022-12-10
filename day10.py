4b6b91494f3b1b2627520e4f46b64c7eac51b663
Robin Michael <Siliconrob@siliconheaven.net>
2022-12-10 13:08:43 -0600

    Part 1 done

--- day10.py
+++ day10.py
@@ -4,113 +4,42 @@ from enum import Enum
 from aocd import get_data
 from parse import parse
 
-
-class Move(Enum):
-    Left: str = 'L'
-    Right: str = 'R'
-    Up: str = 'U'
-    Down: str = 'D'
-
-
 @dataclass
-class Position:
-    X: int
-    Y: int
+class Cycle:
+    Count: int
+    Register: int
+    Instruction: str
+    def signal_strength(self) -> int:
+        return self.Count * self.Register
 
+def part1(instructions: list[str]) -> list[Cycle]:
 
-def move_head(start: Position, direction: Move) -> Position:
-    match direction:
-        case Move.Left:
-            start.X -= 1
-        case Move.Right:
-            start.X += 1
-        case Move.Up:
-            start.Y += 1
-        case Move.Down:
-            start.Y -= 1
-    return Position(start.X, start.Y)
+    current_run = []
 
+    cycle = 0
+    current_value = 1
 
-def expand_moves(input_data: list[str]):
-    all_moves = []
-    for moves in input_data:
-        compact_move = parse('{} {:d}', moves).fixed
-        all_moves.extend([compact_move[0] for index in range(0, compact_move[1])])
-    return all_moves
+    for instruction in instructions:
+        # print(instruction)
+        cycle += 1
+        if instruction == "noop":
+            current_cycle = Cycle(Count=cycle, Register=current_value, Instruction=instruction)
+            current_run.append(current_cycle)
+            continue
+        else:
+            parsed_instruction = parse('{} {}', instruction).fixed
+            current_cycle = Cycle(Count=cycle, Register=current_value, Instruction=instruction)
+            current_run.append(current_cycle)
+            cycle += 1
+            current_cycle = Cycle(Count=cycle, Register=current_value, Instruction=instruction)
+            current_value += int(parsed_instruction[1])
+            current_run.append(current_cycle)
 
-
-def move_tail(head: Position, tail: Position) -> Position:
-    # print(head)
-    if tail.X == head.X and tail.Y == head.Y:
-        return Position(tail.X, tail.Y)
-
-    if tail.X + 1 == head.X or tail.X - 1 == head.X:
-        if tail.Y == head.Y or tail.Y + 1 == head.Y or tail.Y - 1 == head.Y:
-            return Position(tail.X, tail.Y)
-
-    if tail.Y + 1 == head.Y or tail.Y - 1 == head.Y:
-        if tail.X == head.X or tail.X + 1 == head.X or tail.X - 1 == head.X:
-            return Position(tail.X, tail.Y)
-
-    if tail.X == head.X:
-        if tail.Y < head.Y and head.Y - tail.Y == 2:  # up
-            return Position(tail.X, tail.Y + 1)
-        if tail.Y > head.Y and tail.Y - head.Y == 2:  # down
-            return Position(tail.X, tail.Y - 1)
-    if tail.Y == head.Y:
-        if tail.X < head.X and head.X - tail.X == 2:  # right
-            return Position(tail.X + 1, tail.Y)
-        if tail.X > head.X and tail.X - head.X == 2:  # left
-            return Position(tail.X - 1, tail.Y)
-
-    if (tail.X + 1 == head.X or tail.X - 1 == head.X) and (tail.Y + 1 == head.Y or tail.Y - 1 == head.Y):
-        return Position(tail.X, tail.Y)
-
-    if tail.X < head.X:  # move east
-        if tail.Y < head.Y:  # move north
-            return Position(tail.X + 1, tail.Y + 1)
-        if tail.Y > head.Y:  # move south
-            return Position(tail.X + 1, tail.Y - 1)
-    if tail.X > head.X:  # move west
-        if tail.Y < head.Y:  # move north
-            return Position(tail.X - 1, tail.Y + 1)
-        if tail.Y > head.Y:  # move south
-            return Position(tail.X - 1, tail.Y - 1)
-
-
-def part1(input_moves: list[str]):
-    head = Position(0, 0)
-    tail = Position(0, 0)
-    head_visits = [head]
-    tail_visits = [f'{tail.X},{tail.Y}']
-    for move in expand_moves(input_moves):
-        head = move_head(head, Move(move))
-        head_visits.append(head)
-        tail = move_tail(head, tail)
-        tail_visits.append(f'{tail.X},{tail.Y}')
-    return len(set(tail_visits))
+    return current_run
 
 
 def part2(input_moves: list[str]):
-    head = Position(0, 0)
-    head_visits = [head]
-    tails = [Position(0, 0) for x in range(0, 9)]
-    end_tail_visits = [f'{tails[-1].X},{tails[-1].Y}']
-    for move in expand_moves(input_moves):
-        head = move_head(head, Move(move))
-        head_visits.append(head)
-        for tail_index, tail in enumerate(tails):
-            if tail_index == 0:
-                tails[tail_index] = move_tail(head, tail)
-            else:
-                previous = tails[tail_index - 1]
-                tails[tail_index] = move_tail(previous, tail)
-                if tail_index == 8:
-                    end_tail_visits.append(f'{tails[tail_index].X},{tails[tail_index].Y}')
-    return len(set(end_tail_visits))
-
-    return 0
-
+    pass
 
 if __name__ == '__main__':
     # data = [
@@ -125,16 +54,175 @@ if __name__ == '__main__':
     # ]
 
     data = [
-        'R 5',
-        'U 8',
-        'L 8',
-        'D 3',
-        'R 17',
-        'D 10',
-        'L 25',
-        'U 20'
+        'addx 15',
+        'addx -11',
+        'addx 6',
+        'addx -3',
+        'addx 5',
+        'addx -1',
+        'addx -8',
+        'addx 13',
+        'addx 4',
+        'noop',
+        'addx -1',
+        'addx 5',
+        'addx -1',
+        'addx 5',
+        'addx -1',
+        'addx 5',
+        'addx -1',
+        'addx 5',
+        'addx -1',
+        'addx -35',
+        'addx 1',
+        'addx 24',
+        'addx -19',
+        'addx 1',
+        'addx 16',
+        'addx -11',
+        'noop',
+        'noop',
+        'addx 21',
+        'addx -15',
+        'noop',
+        'noop',
+        'addx -3',
+        'addx 9',
+        'addx 1',
+        'addx -3',
+        'addx 8',
+        'addx 1',
+        'addx 5',
+        'noop',
+        'noop',
+        'noop',
+        'noop',
+        'noop',
+        'addx -36',
+        'noop',
+        'addx 1',
+        'addx 7',
+        'noop',
+        'noop',
+        'noop',
+        'addx 2',
+        'addx 6',
+        'noop',
+        'noop',
+        'noop',
+        'noop',
+        'noop',
+        'addx 1',
+        'noop',
+        'noop',
+        'addx 7',
+        'addx 1',
+        'noop',
+        'addx -13',
+        'addx 13',
+        'addx 7',
+        'noop',
+        'addx 1',
+        'addx -33',
+        'noop',
+        'noop',
+        'noop',
+        'addx 2',
+        'noop',
+        'noop',
+        'noop',
+        'addx 8',
+        'noop',
+        'addx -1',
+        'addx 2',
+        'addx 1',
+        'noop',
+        'addx 17',
+        'addx -9',
+        'addx 1',
+        'addx 1',
+        'addx -3',
+        'addx 11',
+        'noop',
+        'noop',
+        'addx 1',
+        'noop',
+        'addx 1',
+        'noop',
+        'noop',
+        'addx -13',
+        'addx -19',
+        'addx 1',
+        'addx 3',
+        'addx 26',
+        'addx -30',
+        'addx 12',
+        'addx -1',
+        'addx 3',
+        'addx 1',
+        'noop',
+        'noop',
+        'noop',
+        'addx -9',
+        'addx 18',
+        'addx 1',
+        'addx 2',
+        'noop',
+        'noop',
+        'addx 9',
+        'noop',
+        'noop',
+        'noop',
+        'addx -1',
+        'addx 2',
+        'addx -37',
+        'addx 1',
+        'addx 3',
+        'noop',
+        'addx 15',
+        'addx -21',
+        'addx 22',
+        'addx -6',
+        'addx 1',
+        'noop',
+        'addx 2',
+        'addx 1',
+        'noop',
+        'addx -10',
+        'noop',
+        'noop',
+        'addx 20',
+        'addx 1',
+        'addx 2',
+        'addx 2',
+        'addx -6',
+        'addx -11',
+        'noop',
+        'noop',
+        'noop'
     ]
 
-    # data = get_data(day=10, year=2022).splitlines()
-    print(f'Part 1: {part1(data)}')
-    print(f'Part 2: {part2(data)}')
+    data = get_data(day=10, year=2022).splitlines()
+
+    start_index = 19
+    increment = 40
+    part1_run = part1(data)
+
+    key_cycles = [
+        part1_run[start_index],
+        part1_run[start_index + increment],
+        part1_run[start_index + increment * 2],
+        part1_run[start_index + increment * 3],
+        part1_run[start_index + increment * 4],
+        part1_run[start_index + increment * 5]
+    ]
+
+    for key_cycle in key_cycles:
+        print(f'[{key_cycle.Count}]({key_cycle.Instruction}, {key_cycle.Register}): {key_cycle.signal_strength()}')
+
+    part1_answer = sum([key_cycle.signal_strength() for key_cycle in key_cycles])
+
+    print(f'Part 1: {part1_answer}')
+
+    # print(f'Part 1: {part1(data)}')
+    # print(f'Part 2: {part2(data)}')
