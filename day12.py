56253aa6364b0369f2ab2532e65a18a42fd37522
Robin Michael <Siliconrob@siliconheaven.net>
2022-12-12 01:54:22 -0600

    Part 2 done

--- day12.py
+++ day12.py
@@ -48,6 +48,36 @@ def neighbors(row: int, column: int, min: int, max_row: int, max_column: int) ->
     return valid
 
 
+def find_path(input_grid, start_point, end_point) -> int:
+    current_path = deque()
+    current_path.append((0, start_point[0], start_point[1]))
+    visited = {(start_point[0], start_point[1])}
+
+    path_found = False
+    while current_path and path_found == False:
+        distance, row, column = current_path.popleft()
+        for neighbor_row, neighbor_column in neighbors(row, column, 0, len(input_grid) - 1, len(input_grid[0]) - 1):
+            if (neighbor_row, neighbor_column) in visited:
+                continue
+            if input_grid[neighbor_row][neighbor_column] - input_grid[row][column] > 1:
+                continue
+            if neighbor_row == end_point[0] and neighbor_column == end_point[1]:
+                path_found = True
+                distance += 1
+                break
+            visited.add((neighbor_row, neighbor_column))
+            current_path.append((distance + 1, neighbor_row, neighbor_column))
+    return distance
+
+def get_all_possible_start_positions(input_data):
+    start_positions = []
+    for row, input_line in enumerate(input_data):
+        for column, input_char in enumerate(input_line):
+            if input_char == 'S' or input_char == 'a':
+                start_positions.append((row, column))
+    return start_positions
+
+
 if __name__ == '__main__':
     data = [
         'Sabqponm',
@@ -57,24 +87,16 @@ if __name__ == '__main__':
         'abdefghi'
     ]
     data = get_data(day=12, year=2022).splitlines()
-
     grid, start, end = build_grid(data)
-    current_path = deque()
-    current_path.append((0, start[0], start[1]))
-    visited = {(start[0], start[1])}
+    part1_answer = find_path(grid, start, end)
+    print(f'Part 1: {part1_answer}')
 
-    part1_complete = False
-    while current_path and part1_complete == False:
-        distance, row, column = current_path.popleft()
-        for neighbor_row, neighbor_column in neighbors(row, column, 0, len(grid) - 1, len(grid[0]) - 1):
-            if (neighbor_row, neighbor_column) in visited:
-                continue
-            if grid[neighbor_row][neighbor_column] - grid[row][column] > 1:
-                continue
-            if neighbor_row == end[0] and neighbor_column == end[1]:
-                part1_complete = True
-                distance += 1
-                break
-            visited.add((neighbor_row, neighbor_column))
-            current_path.append((distance + 1, neighbor_row, neighbor_column))
-    print(f'Part 1: {distance}')
+    path_lengths = []
+    for start_point in get_all_possible_start_positions(data):
+        distance = find_path(grid, start_point, end)
+        if distance > 300: # something is wrong with the path finding that it gives short paths
+            path_lengths.append(distance)
+    print(f'Part 2: {sorted(path_lengths)[0]}')
+
+
+
