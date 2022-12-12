d78f5087fb6f046614ccd8abf2f74b721cbe14c0
Robin Michael <Siliconrob@siliconheaven.net>
2022-12-12 01:19:03 -0600

    Part 1 done

--- day12.py
+++ day12.py
@@ -6,7 +6,8 @@ from aocd import get_data
 from parse import parse
 import networkx as nx
 
-def build_grid(input_data: list[str]) ->list[[int]]:
+
+def build_grid(input_data: list[str]) -> list[[int]]:
     current_grid = []
     start = ()
     end = ()
@@ -24,13 +25,28 @@ def build_grid(input_data: list[str]) ->list[[int]]:
         current_grid.append(grid_row)
     return current_grid, start, end
 
-def neighbors(row: int, column: int) -> list[(int, int)]:
-    return [
+
+def neighbors(row: int, column: int, min: int, max_row: int, max_column: int) -> list[(int, int)]:
+    start_possible = [
         (row + 1, column),
         (row - 1, column),
         (row, column + 1),
         (row, column - 1)
     ]
+
+    valid = []
+    for (new_row, new_column) in start_possible:
+        if new_row < min:
+            continue
+        if new_row > max_row:
+            continue
+        if new_column < min:
+            continue
+        if new_column > max_column:
+            continue
+        valid.append((new_row, new_column))
+    return valid
+
 
 if __name__ == '__main__':
     data = [
@@ -47,17 +63,16 @@ if __name__ == '__main__':
     current_path.append((0, start[0], start[1]))
     visited = {(start[0], start[1])}
 
-    while current_path:
+    part1_complete = False
+    while current_path and part1_complete == False:
         distance, row, column = current_path.popleft()
-        for neighbor_row, neighbor_column in neighbors(row, column):
-
-            if neighbor_row < 0 or neighbor_row >= len(grid) or neighbor_column < 0 or neighbor_column >= len(grid[0]):
-                continue
+        for neighbor_row, neighbor_column in neighbors(row, column, 0, len(grid) - 1, len(grid[0]) - 1):
             if (neighbor_row, neighbor_column) in visited:
                 continue
             if grid[neighbor_row][neighbor_column] - grid[row][column] > 1:
                 continue
             if neighbor_row == end[0] and neighbor_column == end[1]:
+                part1_complete = True
                 distance += 1
                 break
             visited.add((neighbor_row, neighbor_column))
