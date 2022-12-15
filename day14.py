3eb79a3d1fa1e07542d63136d329b833dad017cb
Robin Michael <Siliconrob@siliconheaven.net>
2022-12-14 20:54:01 -0600

    Part 1 done

--- day14.py
+++ day14.py
@@ -7,6 +7,7 @@ from aocd import get_data
 import networkx as nx
 from parse import parse
 
+
 @dataclass(frozen=True)
 class Coordinate:
     X: int
@@ -25,10 +26,6 @@ class Bounds:
 class Line:
     Coordinates: list[Coordinate] = field(default_factory=list)
 
-
-
-
-
     def filled_blocks(self) -> list[Coordinate]:
         blocks = set()
         for coord_pair in list(itertools.pairwise(self.Coordinates)):
@@ -42,72 +39,6 @@ class Line:
                     [Coordinate(next_X, start.Y) for next_X in range(start.X, end.X, -1 if start.X > end.X else 1)])
             blocks.add(end)
         return blocks
-
-
-def neighbors(row: int, column: int, min: int, max_row: int, max_column: int) -> list[(int, int)]:
-    start_possible = [
-        (row + 1, column),
-        (row - 1, column),
-        (row, column + 1),
-        (row, column - 1)
-    ]
-
-    valid = []
-    for (new_row, new_column) in start_possible:
-        if new_row < min:
-            continue
-        if new_row > max_row:
-            continue
-        if new_column < min:
-            continue
-        if new_column > max_column:
-            continue
-        valid.append((new_row, new_column))
-    return valid
-
-
-def find_path(input_grid, start_point, end_point):
-    current_path = deque()
-    current_path.append((0, start_point[0], start_point[1]))
-    visited = {(start_point[0], start_point[1])}
-
-    while current_path:
-        distance, row, column = current_path.popleft()
-        for neighbor_row, neighbor_column in neighbors(row, column, 0, len(input_grid) - 1, len(input_grid[0]) - 1):
-            if (neighbor_row, neighbor_column) in visited:
-                continue
-            if input_grid[neighbor_row][neighbor_column] - input_grid[row][column] > 1:
-                continue
-            if neighbor_row == end_point[0] and neighbor_column == end_point[1]:
-                return distance + 1
-            visited.add((neighbor_row, neighbor_column))
-            current_path.append((distance + 1, neighbor_row, neighbor_column))
-    return None
-
-
-def get_all_possible_start_positions(input_data):
-    start_positions = []
-
-    # top row
-    for current_index, check_position in enumerate(input_data[0]):
-        if check_position == 0:
-            start_positions.append((0, current_index))
-
-    # bottom row
-    for current_index, check_position in enumerate(input_data[-1]):
-        if check_position == 0:
-            start_positions.append((len(input_data) - 1, current_index))
-
-    # left/right columns
-    for row in range(1, len(input_data) - 1):
-        check_position = input_data[row][0]
-        if check_position == 0:
-            start_positions.append((row, 0))
-        check_position = input_data[row][len(input_data[0]) - 1]
-        if check_position == 0:
-            start_positions.append((row, len(input_data[0]) - 1))
-
-    return start_positions
 
 
 def parse_line_coords(line_coords: str) -> Line:
@@ -136,23 +67,68 @@ def get_bounds(input_lines) -> Bounds:
             max_y = current_coords_Y[-1].Y
 
     return Bounds(NorthEast=Coordinate(min_x, min_y),
-           SouthEast=Coordinate(min_x, max_y),
-           NorthWest=Coordinate(max_x, min_y),
-           SouthWest=Coordinate(max_x, max_y))
+                  SouthEast=Coordinate(min_x, max_y),
+                  NorthWest=Coordinate(max_x, min_y),
+                  SouthWest=Coordinate(max_x, max_y))
 
 
-def build_grid(blocks: set(), start: Coordinate, buffer=1):
+def build_grid(blocks: set(), sand: set, start: Coordinate, bounds, buffer=1):
     grid = []
     for y in range(-buffer, ((bounds.SouthEast.Y - bounds.NorthEast.Y) + 1) + buffer):
         grid_row = []
         for x in range(-buffer, ((bounds.NorthWest.X - bounds.NorthEast.X) + 1) + buffer):
             test_coord = Coordinate(x + bounds.NorthEast.X, y + bounds.NorthEast.Y)
             if test_coord in blocks:
-                grid_row.append("+" if test_coord.X == start.X and test_coord.Y == start.Y else "#")
+                if test_coord.X == start.X and test_coord.Y == start.Y:
+                    grid_row.append("+")
+                else:
+                    grid_row.append("#")
+            elif test_coord in sand:
+                grid_row.append("O")
             else:
                 grid_row.append(".")
         grid.append(grid_row)
     return grid
+
+
+def move_sand_part1(position: Coordinate, current_blocks: set, current_sand: set):
+    filled = current_blocks.union(current_sand)
+    down = Coordinate(position.X, position.Y + 1)
+    if down not in filled:
+        return down
+    left = Coordinate(position.X - 1, position.Y + 1)
+    if left not in filled:
+        return left
+    right = Coordinate(position.X + 1, position.Y + 1)
+    if right not in filled:
+        return right
+    return None
+
+
+def print_grid(input_grid):
+    for grid_row in input_grid:
+        print("".join(grid_row))
+
+
+def part1(input_lines):
+    bounds = get_bounds(input_lines)
+    start = Coordinate(500, 0)
+    print(bounds)
+    blocks = set()
+    blocks.add(Coordinate(500, 0))
+    for line in lines:
+        blocks = blocks.union(list(line))
+    filled_sand = set()
+    current_position = start
+    while current_position.Y < bounds.SouthEast.Y:
+        new_position = move_sand_part1(current_position, blocks, filled_sand)
+        if new_position is None:
+            filled_sand.add(current_position)
+            current_position = start
+        else:
+            current_position = new_position
+    print_grid(build_grid(blocks, filled_sand, start, bounds))
+    return len(filled_sand)
 
 
 if __name__ == '__main__':
@@ -161,43 +137,8 @@ if __name__ == '__main__':
         '503, 4 -> 502, 4 -> 502, 9 -> 494, 9'
     ]
 
-    #data = get_data(day=14, year=2022).splitlines()
+    # data = get_data(day=14, year=2022).splitlines()
 
     lines = [parse_line_coords(line_coords) for line_coords in data]
-    bounds = get_bounds(lines)
+    print(f'Part 1: {part1(lines)}')
 
-    start = Coordinate(500, 0)
-    print(bounds)
-
-
-
-    blocks = set()
-    blocks.add(Coordinate(500, 0))
-    for line in lines:
-        blocks = blocks.union(list(line))
-
-    the_grid = build_grid(blocks, start)
-
-    for grid_row in the_grid:
-        print("".join(grid_row))
-
-
-    #print(f'{x_range} {y_range}')
-
-    # Bounds(NorthEast=Coordinate(bounds.NorthEast.X - 10))
-
-
-
-    # for line in lines:
-    #     print(line)
-
-    # grid, start, end = build_grid(data)
-    # part1_answer = find_path(grid, start, end)
-    # print(f'Part 1: {part1_answer}')
-    #
-    # path_lengths = []
-    # for start_point in get_all_possible_start_positions(grid):
-    #     distance = find_path(grid, start_point, end)
-    #     if distance is not None:
-    #         path_lengths.append(distance)
-    # print(f'Part 2: {sorted(path_lengths)[0]}')
