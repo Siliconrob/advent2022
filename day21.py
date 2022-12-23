8382ee1dfdf483b9f66eb2ae7c12984a9a14d269
Robin Michael <Siliconrob@siliconheaven.net>
2022-12-23 13:37:41 -0600

    Remove inner loop to be cleaner about evaluating everything

--- day21.py
+++ day21.py
@@ -1,5 +1,7 @@
 from aocd import get_data
 from parse import parse
+import sympy
+
 def parse_input_lines(data):
     monkeys = {}
     for input_line in data:
@@ -15,15 +17,24 @@ def parse_input_lines(data):
 def part1(monkeys_to_search, search_monkey_id: str) -> int:
     while isinstance(monkeys_to_search[search_monkey_id], str):
         for key, value in monkeys_to_search.items():
-            if isinstance(value, int) or isinstance(value, float):
-                for k, v in monkeys_to_search.items():
-                    if isinstance(v, str):
-                        monkeys_to_search[k] = v.replace(key, str(value))
-                        try:
-                            monkeys_to_search[k] = eval(v)
-                            break
-                        except:
-                            continue
+            if isinstance(value, int):
+                continue
+            else:
+                left_side, operand, right_side = parse('{} {} {}', value)
+                if left_side.lower().islower() and isinstance(monkeys_to_search[left_side], int):
+                    left_side = monkeys_to_search[left_side]
+                if right_side.lower().islower() and isinstance(monkeys_to_search[right_side], int):
+                    right_side = monkeys_to_search[right_side]
+                new_value = f'{left_side} {operand} {right_side}'
+                monkeys_to_search[key] = new_value
+                if new_value.lower().islower():
+                    continue
+                else:
+                    try:
+                        monkeys_to_search[key] = int(eval(new_value))
+                        break
+                    except:
+                        continue
     return monkeys_to_search[search_monkey_id]
 
 
@@ -45,11 +56,12 @@ if __name__ == '__main__':
         'drzm: hmdt - zczc',
         'hmdt: 32'
     ]
-    data = get_data(day=21, year=2022).splitlines()
+    # data = get_data(day=21, year=2022).splitlines()
+
+    # sympy.solve(sympy.Integer('4') - sympy.Integer())
 
     screaming_monkeys = parse_input_lines(data)
     root_monkey_id = 'root'
 
     part1_answer = part1(screaming_monkeys, root_monkey_id)
     print(f'Part 1: {part1_answer}')
-
