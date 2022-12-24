ebf1330797dc95ac2bca7c27d5ea2b42816a8d62
Robin Michael <Siliconrob@siliconheaven.net>
2022-12-23 23:46:49 -0600

    Update and add in part 2

--- day21.py
+++ day21.py
@@ -38,6 +38,48 @@ def part1(monkeys_to_search, search_monkey_id: str) -> int:
     return monkeys_to_search[search_monkey_id]
 
 
+def algebra_solve(input):
+    left_side, operand, right_side = parse('{} {} {}', input)
+    if isinstance(left_side, str) or isinstance(right_side, str):
+        return False
+    return True
+
+def part2(monkeys_to_search, search_monkey_id, human_id) -> int:
+
+    # Setup functions to run for solver
+    math = {
+        "+": lambda x, y: x + y,
+        "-": lambda x, y: x - y,
+        "*": lambda x, y: x * y,
+        "/": lambda x, y: x / y
+    }
+
+    # assign the variable
+    symbolic_monkeys = { human_id: sympy.Symbol("x") }
+
+    # you can keep appending to a list as it loops through to run over and over
+    for next_line in monkeys_to_search:
+        current_key, current_value = parse('{}: {}', next_line)
+        if current_key in symbolic_monkeys:
+            continue
+        if not current_value.lower().islower():
+            symbolic_monkeys[current_key] = sympy.Integer(current_value) # make sure solver knows this is an integer
+        else:
+            left_side, fn, right_side = parse('{} {} {}', current_value)
+            if left_side in symbolic_monkeys and right_side in symbolic_monkeys:
+                if current_key == search_monkey_id:
+                    # You want solver to find your varilable so subtract the 2 sides to get what x needs to be to balance
+                    # the equations, only 1 value so pop it off
+                    solved = sympy.solve(symbolic_monkeys[left_side] - symbolic_monkeys[right_side]).pop()
+                    return solved
+                # Build the set of algebra rules as lambdas with say x - 3 (value) etc
+                symbolic_monkeys[current_key] = math[fn](symbolic_monkeys[left_side], symbolic_monkeys[right_side])
+            else:
+                # line isn't expressible in single variable yet so add it back to the list so it will be reprocessed
+                monkeys_to_search.append(next_line)
+
+    return None
+
 if __name__ == '__main__':
     data = [
         'root: pppw + sjmn',
@@ -56,12 +98,15 @@ if __name__ == '__main__':
         'drzm: hmdt - zczc',
         'hmdt: 32'
     ]
-    # data = get_data(day=21, year=2022).splitlines()
-
-    # sympy.solve(sympy.Integer('4') - sympy.Integer())
+    data = get_data(day=21, year=2022).splitlines()
 
     screaming_monkeys = parse_input_lines(data)
     root_monkey_id = 'root'
 
     part1_answer = part1(screaming_monkeys, root_monkey_id)
     print(f'Part 1: {part1_answer}')
+
+    human_id = 'humn'
+
+    part2_answer = part2(data, root_monkey_id, human_id)
+    print(f'Part 2: {part2_answer}')
